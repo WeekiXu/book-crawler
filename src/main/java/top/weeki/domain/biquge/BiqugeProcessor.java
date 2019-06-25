18ae4ec7ab12be1491a2f4e18efa51026b82f1ab
weeki <xujiwei@weiresearch.com>
2019-06-25 20:04:43 +0800

    @WeekiXu/部分修正

--- src/main/java/top/weeki/domain/biquge/BiqugeProcessor.java
+++ src/main/java/top/weeki/domain/biquge/BiqugeProcessor.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 import top.weeki.domain.dingdian.DingdianJsonFilePipeline;
@@ -19,14 +20,17 @@ import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
-public class BiqugeProcessor implements PageProcessor {
+public class BiqugeProcessor
+    implements PageProcessor {
     // 部分一：抓取网站的相关配置，包括编码、抓取间隔、重试次数等
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
-    public static String indexRegix = "https://www.biquge.cc/html/\\d+/\\d+/";
-    public static String detailRegix = "https://www.biquge.cc/html/\\d+/\\d+/\\d+.html";
+    public static String indexRegix = "https://www.biquge5200.cc/\\d+_\\d+/";
+    public static String detailRegix =
+        "https://www.biquge5200.cc/\\d+_\\d+/\\d+.html";
 
     @Override
-    public void process(Page page) {
+    public void process(
+        Page page) {
 
         // 2. 抽取文章页
         if (page.getUrl().regex(detailRegix).match()) {
@@ -39,24 +43,28 @@ public class BiqugeProcessor implements PageProcessor {
         }
     }
 
-    private void fetchPageDetail(Page page) {
-        String bookName = page.getHtml().xpath("//div[@class='con_top']/a[2]/@title").toString();
-        String title = page.getHtml().xpath("//div[@class='bookname']/h1/text()").toString();
-        String content = page.getHtml().xpath("//div[@id='content']/text()").toString().replaceAll("&nbsp;", "")
-            .replaceAll("　", "").replaceAll("    ", "\n    ");
+    private void fetchPageDetail(
+        Page page) {
+        String bookName = page.getHtml()
+            .xpath("//div[@class='con_top']/a[3]/text()").toString();
+        String title = page.getHtml()
+            .xpath("//div[@class='bookname']/h1/text()").toString();
+        List<String> contents =
+            page.getHtml().xpath("//div[@id='content']/p/text()").all();
         String url = page.getRequest().getUrl().toString();
         int index = Integer.parseInt(url.substring(url.lastIndexOf("=") + 1));
         page.putField("bookName", bookName);
         page.putField("index", index);
         page.putField("title", title.trim());
-        page.putField("content", "    " + content.trim());
+        page.putField("content", contents);
     }
 
-    private void fetchPageList(Page page) {
+    private void fetchPageList(
+        Page page) {
         String indexUrl = page.getUrl().toString();
         String pageRegix = indexUrl + "\\d+.html";
         List<String> list = page.getHtml().links().regex(pageRegix).all();
-//        list = list.subList(0, 10);
+        // list = list.subList(0, 10);
         for (int i = 0; i < list.size(); i++) {
             list.set(i, list.get(i) + "?index=" + i);
         }
@@ -68,9 +76,12 @@ public class BiqugeProcessor implements PageProcessor {
         return site;
     }
 
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(
+        String[] args)
+        throws InterruptedException, IOException {
         String path = "D:\\tmp\\book\\";
-        Spider spider = Spider.create(new BiqugeProcessor()).addUrl("https://www.biquge.cc/html/129/129557/")
+        Spider spider = Spider.create(new BiqugeProcessor())
+            .addUrl("https://www.biquge5200.cc/0_87/")
             .addPipeline(new DingdianJsonFilePipeline("D:\\tmp\\book\\"))
             // 开启5个线程抓取
             .thread(100);
@@ -83,12 +94,20 @@ public class BiqugeProcessor implements PageProcessor {
             Thread.sleep(10000);
         }
         // 汇总txt
-        String bookName = "异常生物见闻录";
-        Stream<Path> files = Files.list(new File(path + spider.getUUID() + "\\" + bookName).toPath());
-        // Stream<Path> files = Files.list(new File("D:\\tmp\\book\\www.23us.so\\仙界归来").toPath());
+        String bookName = "武极天下";
+        Stream<Path> files = Files
+            .list(new File(path
+                + spider.getUUID()
+                + "\\"
+                + bookName).toPath());
+        // .list(new File(path + "www.biquge5200.cc" + "\\" + bookName)
+        // .toPath());
+        // Stream<Path> files = Files.list(new
+        // File("D:\\tmp\\book\\www.23us.so\\仙界归来").toPath());
 
         List<Integer> indexList = new ArrayList<>();
-        files.forEach(path1 -> indexList.add(Integer.parseInt(path1.getFileName().toString().replace(".json", ""))));
+        files.forEach(path1 -> indexList.add(Integer
+            .parseInt(path1.getFileName().toString().replace(".json", ""))));
         indexList.sort(Integer::compareTo);
 
         File resultFile = new File(path + bookName + ".txt");
@@ -98,15 +117,31 @@ public class BiqugeProcessor implements PageProcessor {
         PrintWriter printWriter = new PrintWriter(new FileWriter(resultFile));
         for (Integer index : indexList) {
             List<String> lines = Files
-                .readAllLines(new File(path + spider.getUUID() + "\\" + bookName + "\\" + index + ".json").toPath());
-            // List<String> lines = Files.readAllLines(new File(path + "www.23us.so\\" + bookName + "\\" + index +
+                .readAllLines(new File(path
+                    + spider.getUUID()
+                    // + "www.biquge5200.cc"
+                    + "\\"
+                    + bookName
+                    + "\\"
+                    + index
+                    + ".json").toPath());
+            // List<String> lines = Files.readAllLines(new File(path +
+            // "www.23us.so\\" + bookName + "\\" + index +
             // ".json").toPath());
             if (lines.isEmpty()) {
                 continue;
             }
             JSONObject book = JSON.parseObject(lines.get(0));
             printWriter.println(book.getString("title"));
-            printWriter.println(book.getString("content"));
+            try {
+                JSONArray contents = book.getJSONArray("content");
+                for (int i = 0; i < contents.size(); i++) {
+                    printWriter.println(contents.getString(i));
+                }
+
+            } catch (Exception e) {
+                System.out.println(book);
+            }
             printWriter.flush();
         }
         printWriter.close();
